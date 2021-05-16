fa429815356232458c6ec0e328a3ebf22028f56e
Fabian Holzwarth <holzwarth.fabian@freenet.de>
2021-05-16 22:41:09 +0200

    [TASK] Initialize

--- WebuApp.php
+++ WebuApp.php
@@ -5,4 +5,15 @@ namespace CustomWebuApp;
 use webu\system\Core\Base\Module\BaseModule;
 
 class WebuApp extends BaseModule {
+
+    public function install()
+    {
+        // TODO: Implement install() method.
+    }
+
+    public function uninstall()
+    {
+        // TODO: Implement uninstall() method.
+    }
+
 }   
